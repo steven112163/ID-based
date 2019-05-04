1e2c8822c77e0171b93f3ccbf1fc7851b2c4c131
steven112163 <43545147+steven112163@users.noreply.github.com>
2019-05-04 21:24:54 +0800

    Correct typo

--- Login/db.php
+++ Login/db.php
@@ -2,7 +2,7 @@
 $link = mysqli_connect('192.168.44.128','root','root','portal');
 
 if (!$link) {
-  die('Could not connect:' .mysquli_error());
+  die('Could not connect:' .mysqli_error());
 }
 // echo 'Connected successfully';
 mysql_query($link, "SET NAMES 'utf8'");
